2bfbde883f0966f659d935addc90131186af06b6
DESKTOP-TKG5QQM\SangyeopHan <super7615@naver.com>
2025-02-19 20:58:58 +0900

    [instagram_clone] - like 상태변화, listview feed 만들기

--- instagram_clone/lib/feed.dart
+++ instagram_clone/lib/feed.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
   const Feed({
     super.key,
   });
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  bool isFavorite = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +27,15 @@ class Feed extends StatelessWidget {
         Row(
           children: [
             IconButton(
-              icon: Icon(CupertinoIcons.heart, color: Colors.black),
-              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.heart,
+                color: isFavorite ? Colors.pink : Colors.black,
+              ),
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
             ),
             IconButton(
               icon: Icon(CupertinoIcons.chat_bubble, color: Colors.black),
